a9aa4c14d5fb83b81ffba66e19c1b78c035b3fed
AlbertRyu <btp_albert@photons.com.tw>
2025-09-12 11:44:52 +0800

    Add solution numIslandsAddLand

--- number_of_islands_add_land.go
+++ number_of_islands_add_land.go
@@ -1,52 +1,57 @@
 package leetcode
 
-const (
-	WaterInt = 0
-	LandInt  = 999
-)
-
 // union
+func union(p []int, a, b int) {
+	parentA := findParent(p, a)
+	parentB := findParent(p, b)
+	if parentA != parentB {
+		p[b] = parentA
+	}
+}
+
 // parent
+func findParent(p []int, idx int) int {
+	if p[idx] == idx {
+		return idx
+	}
+	return findParent(p, p[idx])
+}
 
 func numIslandsAddLand(m, n int, positions [][]int) []int {
 	// build water grid
-	grid := make([][]byte, m)
+	grid := make([][]int, m)
 	for i, _ := range grid {
-		grid[i] = make([]byte, n)
+		grid[i] = make([]int, n)
 	}
+	landParent := make([]int, len(positions)+1)
 
-	islandNum := 0
-	ans := make([]int, len(positions))
+	ans := make([]int, 0)
+	islandCount := 0
 	for i, p := range positions {
-		// check
+		// set island
+		landNum := i + 1
+		x := p[0]
+		y := p[1]
+		grid[x][y] = landNum
+		landParent[landNum] = landNum
 
-		grid[p[0]][p[1]] = LandInt
+		// union island
+		islandCount++
+		for i := 0; i < 4; i++ {
+			tmpX, tmpY := x+dx[i], y+dy[i]
+			if tmpX >= 0 && tmpX < m &&
+				tmpY >= 0 && tmpY < n &&
+				grid[tmpX][tmpY] > 0 {
 
-		islandNum++
-	}
-	return ans
-}
-
-var islandNumMap = map[int][][]int{} // islandNum:landPositions
-
-func markAdjacentMinNumLand(x, y int, grid [][]int) {
-	adjacentLandPositions := make([][]int, 0)
-	adjacentMinNumLand := 0
-	for i := 0; i < 4; i++ {
-		tmpX, tmpY := x+dx[i], y+dy[i]
-		if tmpX >= 0 && tmpX < m &&
-			tmpY >= 0 && tmpY < n &&
-			grid[tmpY][tmpX] > 0 {
-			adjacentLandPositions = append(adjacentLandPositions, []int{tmpY, tmpX})
-			if adjacentMinNumLand == 0 || grid[tmpY][tmpX] < adjacentMinNumLand {
-				adjacentMinNumLand = grid[tmpY][tmpX]
+				tmpLandNum := grid[tmpX][tmpY]
+				if findParent(landParent, landNum) != findParent(landParent, tmpLandNum) {
+					union(landParent, tmpLandNum, landNum)
+					islandCount--
+				}
 			}
 		}
-	}
 
-	if adjacentMinNumLand > 0 {
-		for _, position := range adjacentLandPositions {
-			grid[position[0]][position[1]]
-		}
+		ans = append(ans, islandCount)
 	}
+	return ans
 }
