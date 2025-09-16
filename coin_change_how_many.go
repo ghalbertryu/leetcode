445de3260c055fd70e130abaf4a7f69967f082f5
AlbertRyu <btp_albert@photons.com.tw>
2025-09-16 11:53:52 +0800

    Add solution coinChangeHowManyWays

---
+++ coin_change_how_many.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func coinChangeHowManyWays(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+
+	for _, coin := range coins {
+		for i := 0; i <= amount; i++ {
+			if i-coin >= 0 {
+				dp[i] = dp[i] + dp[i-coin]
+				// dp[i]: 原本湊 i 數量可以的方式數量
+				// dp[i-coin] 種: 湊 i 數量, 使用 1個coin 時的方式數量
+			}
+		}
+	}
+	return dp[amount]
+}
---
+++ coin_change_how_many_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"log"
+	"testing"
+)
+
+func TestCoinChangeHowManyWays(t *testing.T) {
+	log.Println(coinChangeHowManyWays([]int{1, 2, 5}, 1)) // 1
+	log.Println(coinChangeHowManyWays([]int{1, 2, 5}, 2)) // 2
+	log.Println(coinChangeHowManyWays([]int{1, 2}, 4))    // 3
+}
