32eea02d4453979d79efe65a0bfabe0b9dd2b449
AlbertRyu <btp_albert@photons.com.tw>
2025-09-18 17:34:32 +0800

    Add solution maxSubArrayPrefixSum

--- 53_maximum_subarray.go
+++ 53_maximum_subarray.go
@@ -1,6 +1,47 @@
 package leetcode
 
-import "math"
+import (
+	"log"
+	"math"
+)
+
+func maxSubArrayPrefixSum(nums []int) int {
+	ans := math.MinInt64
+	prefixSum := 0
+	minPrefixSum := 0
+
+	for i := 0; i < len(nums); i++ {
+		prefixSum += nums[i]
+		maxSum := prefixSum - minPrefixSum
+		if maxSum > ans {
+			ans = maxSum
+		}
+		if prefixSum < minPrefixSum {
+			minPrefixSum = prefixSum
+		}
+	}
+
+	return ans
+}
+
+func prefixSumExample(nums []int) {
+	prefixSum := make([]int, len(nums)+1)
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			prefixSum[i] = nums[i]
+			continue
+		}
+		prefixSum[i] = prefixSum[i-1] + nums[i]
+	}
+
+	log.Println(prefixSum)
+
+	// sum of idx range: [2~5]
+	log.Println(prefixSum[5] - prefixSum[1])
+
+	// sum of idx range: [i~j]
+	//log.Println(prefixSum[j] - prefixSum[i-1])
+}
 
 func maxSubArraySecond(nums []int) int {
 	ans := math.MinInt64
--- 53_maximum_subarray_test.go
+++ 53_maximum_subarray_test.go
@@ -6,8 +6,12 @@ import (
 )
 
 func TestMaxSubArray(t *testing.T) {
-	log.Println(maxSubArraySecond([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})) // 6
-	log.Println(maxSubArraySecond([]int{1}))                             // 1
-	log.Println(maxSubArraySecond([]int{1, 2}))                          // 3
+	log.Println(maxSubArrayPrefixSum([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})) // 6
+	log.Println(maxSubArraySecond([]int{1}))                                // 1
+	log.Println(maxSubArraySecond([]int{1, 2}))                             // 3
 	//log.Println(maxSubArray([]int{-2, 1, -3, 400, -1, 2, 1, -5, 400}))
+}
+
+func TestPrefixSumExample(t *testing.T) {
+	prefixSumExample([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4})
 }
