fa6f120c385f792f11bf5b9e498a9734a7d97b74
AlbertRyu <btp_albert@photons.com.tw>
2025-09-19 15:57:13 +0800

    Add solution of meeting_room

---
+++ meeting_room.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"slices"
+)
+
+// 每個meeting有一個開始時間(start)與結束時間(end)
+// 問這些meeting時間有沒有衝突，衝突的意思為同一個時間點需要同時出現在兩場meeting
+// 若某一場meeting的結束時間與下一場meeting的開始時間相同不視為衝突
+
+type MeetingInterval struct {
+	startTime int
+	endTime   int
+}
+
+func compareTo(a MeetingInterval, b MeetingInterval) int {
+	return a.startTime - b.startTime
+}
+
+func meetingNotConflict(meetings []MeetingInterval) bool {
+	slices.SortFunc(meetings, compareTo)
+
+	endTime := -1
+	for _, meeting := range meetings {
+		if meeting.startTime < endTime {
+			return false
+		}
+		endTime = meeting.endTime
+	}
+	return true
+}
---
+++ meeting_room_test.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import (
+	"log"
+	"testing"
+)
+
+func TestMeetingNotConflict(t *testing.T) {
+	meetings0 := []MeetingInterval{
+		{1, 5},
+		{7, 9},
+	}
+	log.Println(meetingNotConflict(meetings0))
+
+	meetings1 := []MeetingInterval{
+		{1, 5},
+		{7, 9},
+		{2, 3},
+	}
+	log.Println(meetingNotConflict(meetings1))
+
+	meetings2 := []MeetingInterval{
+		{1, 5},
+		{5, 8},
+		{7, 9},
+	}
+	log.Println(meetingNotConflict(meetings2))
+}
