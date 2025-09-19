6d75c286b66e237c310604aa7e8c8e9e0acbc18e
AlbertRyu <btp_albert@photons.com.tw>
2025-09-19 17:35:01 +0800

    Add solution minMeetingRoomOptimize

--- meeting_room_ii.go
+++ meeting_room_ii.go
@@ -1,17 +1,56 @@
 package leetcode
 
-import "slices"
+import (
+	"slices"
+)
 
-type Room struct {
-	start int
-	end   int
+type MeetingEvent struct {
+	eventTime int
+	isStart   bool
 }
 
 // 給定一些meeting的時間，每個meeting有一個開始時間(start)與結束時間(end)
 // 問要完成這些meeting最少需要幾間會議室
 // 若同一個時間點有兩場meeting在進行，則這兩場meeting需要用不同間會議室
 // 若某一場meeting的結束時間與下一場meeting的開始時間相同，則他們可以使用同一間會議室
-func minMeetingRoom(meetings []MeetingInterval) int {
+func minMeetingRoomOptimize(meetings []MeetingInterval) int {
+	meetingEvents := make([]MeetingEvent, 0)
+	for _, meeting := range meetings {
+		meetingEvents = append(meetingEvents, MeetingEvent{
+			eventTime: meeting.startTime,
+			isStart:   true,
+		})
+		meetingEvents = append(meetingEvents, MeetingEvent{
+			eventTime: meeting.endTime,
+			isStart:   false,
+		})
+	}
+
+	slices.SortFunc(meetingEvents, func(a, b MeetingEvent) int {
+		return a.eventTime - b.eventTime
+	})
+
+	ans := 0
+	roomCount := 0
+	for _, event := range meetingEvents {
+		if event.isStart {
+			roomCount++
+		} else {
+			roomCount--
+		}
+		if roomCount > ans {
+			ans = roomCount
+		}
+	}
+	return ans
+}
+
+type Room struct {
+	start int
+	end   int
+}
+
+func minMeetingRoomFirst(meetings []MeetingInterval) int {
 	slices.SortFunc(meetings, compareTo)
 	rooms := make([]Room, 0)
 
--- meeting_room_ii_test.go
+++ meeting_room_ii_test.go
@@ -10,19 +10,19 @@ func TestMinMeetingRoom(t *testing.T) {
 		{1, 5},
 		{7, 9},
 	}
-	log.Println(minMeetingRoom(meetings0))
+	log.Println(minMeetingRoomOptimize(meetings0))
 
 	meetings1 := []MeetingInterval{
 		{1, 5},
 		{7, 9},
 		{2, 3},
 	}
-	log.Println(minMeetingRoom(meetings1))
+	log.Println(minMeetingRoomOptimize(meetings1))
 
 	meetings3 := []MeetingInterval{
 		{1, 10},
 		{2, 8},
 		{3, 5},
 	}
-	log.Println(minMeetingRoom(meetings3))
+	log.Println(minMeetingRoomOptimize(meetings3))
 }
